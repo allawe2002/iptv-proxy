326643b6943a1280687e5b193d445228b9939467
allawe2002 <allawe2002@yahoo.com>
2025-07-17 15:31:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ hls_template = '''
 
      <div class="channel-container">
         <img src="/static/logos/nbn.png" alt="TLC Logo" width="100">
-        <video id="player8" width="320" height="180" controls poster="/static/logos/nbn.png"></video>
+        <video id="player8" width="320" height="180" controls poster="/static/logos/tlc.png"></video>
         <div class="channel-info">
             <h3>🍑 ⋆ 🍉  🎀  𝒯𝐿𝒞 🍩𝒮𝒩  🎀  🍉 ⋆ 🍑</h3>
             <button class="control-btn" onclick="toggleStream('player8', 'http://404focusnotfound.com:80/play/live.php?mac=00:1B:79:3E:05:F4&stream=925256&extension=m3u8&play_token=72E19255AF0D91DD0AD713012761C715')">Play/Stop</button>
