13aba19e63462153b728ed9e28791d30aec45a10
allawe2002 <allawe2002@yahoo.com>
2025-07-22 10:23:35 +0400

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ hls_template = '''
         <video id="player7" width="320" height="180" controls poster="/static/logos/nbn.png"></video>
         <div class="channel-info">
             <h3>⋆`  🎀  𝒩𝐵𝒩 𝒯𝒱  🎀  `⋆</h3>
-            <button class="control-btn" onclick="toggleStream('player7', '/proxy/?url=http://5.9.119.146:8883/nbn/index.m3u8')">Play/Stop</button>
+            <button class="control-btn" onclick="toggleStream('player7', 'http://5.9.119.146:8883/nbn/index.m3u8')">Play/Stop</button>
      </div>
      </div>
 
