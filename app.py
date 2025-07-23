4410555d291d9fa80c42608fd7cf93f998d25467
allawe2002 <allawe2002@yahoo.com>
2025-07-23 13:33:22 +0400

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ function toggleStream(playerId, streamUrl) {
         <img src="/static/logos/nbn.png" alt="NBN Logo" width="100">
         <video id="player7" width="320" height="180" controls poster="/static/logos/nbn.png"></video>
         <div class="channel-info">
-            <h3>⋆  🎀  𝒩𝐵𝒩 𝒯𝒱  🎀  ⋆</h3>
+            <h3> 🎀  𝒩𝐵𝒩 𝒯𝒱  🎀 </h3>
             <button class="control-btn" onclick="toggleStream('player7', 'https://samaflix.com:12103/channel5/tracks-v2a1/mono.m3u8')">Play/Stop</button>
      </div>
      </div>
