f88c0fbfd81fff7a7af8baf09aa35f3bff111e63
allawe2002 <allawe2002@yahoo.com>
2025-07-25 12:23:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ function toggleYouTube(containerId, videoId) {
     <div id="youtube-container-cbc" style="width: 320px; height: 180px; background-color: #000;"></div>
     <div class="channel-info">
         <h3>📺 𝒞𝐵𝒞 𝒩𝑒𝓌𝓈 (𝒴o𝓊𝒯𝓊𝒷𝑒) </h3>
-        <button class="control-btn" onclick="toggleYouTube('youtube-container-cbc', 'W44Vmriu7to')">Play/Stop</button>
+        <button class="control-btn" onclick="toggleYouTube('youtube-container-cbc', 'X0alCXqp148')">Play/Stop</button>
     </div>
 </div>
 
